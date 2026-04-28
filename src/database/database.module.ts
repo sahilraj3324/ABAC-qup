5b7ff303a7e2507bda12fdc3c25250537d8b2f53
Sahil raj <rajsahil3324@gmail.com>
2026-04-28 13:18:16 +0530

    db connection

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,20 +1,27 @@
 import { Module, Global } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Pool } from 'pg';
 
 export const DATABASE_POOL = 'DATABASE_POOL';
 
 @Global()
 @Module({
+  imports: [ConfigModule],
   providers: [
     {
       provide: DATABASE_POOL,
-      useFactory: () =>
-        new Pool({
-          connectionString: process.env.DATABASE_URL,
-          ssl: process.env.DATABASE_SSL === 'true' ? { rejectUnauthorized: false } : false,
-        }),
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        const url = config.get<string>('DATABASE_URL');
+        const ssl = config.get<string>('DATABASE_SSL') === 'true';
+        return new Pool({
+          connectionString: url,
+          ssl: ssl ? { rejectUnauthorized: false } : false,
+        });
+      },
     },
   ],
   exports: [DATABASE_POOL],
 })
 export class DatabaseModule {}
+
